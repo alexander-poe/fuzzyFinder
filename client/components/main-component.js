f4de13edb5e3f3bd19b5d28e37ab024f75e71324
alexpoe <alexpoe22@gmail.com>
2017-04-30 12:46:13 -0700

    Update main-component.js

--- client/components/main-component.js
+++ client/components/main-component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import info from '../data/info';
 import fuzzyFind from '../logic/fuzzyFind';
-import Header from './Header';
+import header from './Header';
 import Searchbar from './searchbar';
 import Results from './results';
 
